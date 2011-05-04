08287a19cc9922dc0691fca7541f37c9980d8377
Ivan Kozik <ivan@ludios.org>
2011-05-04 14:14:05 +0000

    Write some better docstrings

--- brequire.py
+++ brequire.py
@@ -2,7 +2,11 @@
 Any module that needs non-Python-module files to work correctly should import
 this module, and call requireFile at module scope like this:
 
-requireFile(FilePath(__file__).sibling('some_file'))
+requireFile(FilePath(__file__).sibling('some_file').path)
+
+or if you don't want to use filepath.FilePath:
+
+requireFile(os.path.join(os.path.dirname(__file__), 'some_file'))
 
 This allows Deepfreeze to know which static files are necessary for each
 individual module.  (The distutils setup.py approach of setting package_data
@@ -20,11 +24,22 @@ allRequires = set()
 
 def requireFile(fpath):
 	"""
+	Declare that C{fpath} is necessary for the program to work.  C{fpath}
+	should be a file inside one of your C{sys.path} directories.
+
 	@param fpath: a C{str} or C{unicode} representing an absolute file path.
 	"""
 	allRequires.add(fpath)
 
 
 def requireFiles(fpaths):
+	"""
+	Declare that every path in C{fpaths} is necessary for the program to
+	work.  Each path should be a file inside one of your C{sys.path}
+	directories.
+
+	@param fpaths: an iterable of C{str} or C{unicode}, each representing an
+		absolute file path.
+	"""
 	for fpath in fpaths:
 		requireFile(fpath)
