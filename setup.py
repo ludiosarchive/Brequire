956699bde81b78f5b4ff4200d13ba37b6f897f44
Ivan Kozik <ivan@ludios.org>
2011-05-04 14:16:16 +0000

    Add LICENSE; add author and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,15 @@ setup(
 	version=brequire.__version__,
 	description="A small module that collects 'I need this non-Python " +
 		"file to operate correctly' declarations.",
+	url="https://github.com/ludios/Brequire",
+	author="Ivan Kozik",
+	author_email="ivan@ludios.org",
+	classifiers=[
+		'Programming Language :: Python :: 2',
+		'Development Status :: 5 - Production/Stable',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+	],
 	py_modules=['brequire'],
 )
